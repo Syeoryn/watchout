6c08538e2ce78ca1fe048fb80e2c20b3284c18d0
Hack Reactor Students <syeoryn@gmail.com>
2014-04-26 00:18:01 -0700

    Implements basic collision detection and score counts.

--- watchout.js
+++ watchout.js
@@ -7,12 +7,13 @@ var options ={
   enemyRadius: 10
 };
 
-var enemyIds = [];
-
-for(var enemy = 1; enemy <= options.enemies; enemy++){
-  enemyIds.push(enemy);
+var stats = {
+  current: 0,
+  highScore: 0,
+  collisions: 0
 }
 
+// Define game board
 var axes = {
   x: d3.scale.linear().domain([0,100]).range([0,options.width]),
   y: d3.scale.linear().domain([0,100]).range([0,options.height])
@@ -24,9 +25,67 @@ var board = d3.select("body").append("svg")
               .style("height", options.height)
               .style("background-color", "black");
 
+
+// Create player
+var playerSpecs = {
+  shape: "path",
+  path:   "M 9  1"
+        + "L 17 26"
+        + "L 9  22"
+        + "L 1  26"
+        + "L 9  1",
+  id: 0
+};
+
+// Add player to board
+var playerContainer = board.append("svg:svg")
+                           .attr("class","playerContainer")
+                           .attr("width","20")
+                           .attr("height","28")
+                           .attr("x",options.width/2)
+                           .attr("y",options.height/2);
+
+var player = board.selectAll(".player").data([player]);
+
+playerContainer.append("svg:" + playerSpecs.shape)
+               .attr("d",playerSpecs.path)
+               .attr("class","player")
+               .attr("stroke","#00D000")
+               .attr("stroke-opacity",1e-6)
+               .attr("stroke-width", 1e-6)
+               .attr("fill","#00D000")
+               .attr("fill-opacity",1e-6)
+             .transition()
+               .attr("fill-opacity",0.25)
+               .duration(1500)
+               .attr("stroke-opacity",1)
+               .attr("stroke-width",2);
+
+// Give player click and drag capability
+var drag = d3.behavior.drag().on("drag",function(){
+  d3.select(this)
+    .attr("x",function(){return d3.event.x;})
+    .attr("y",d3.event.y);
+});
+
+d3.select(".playerContainer").call(drag);
+
+
+// Create enemies
+var makeEnemies = function(){
+  var enemyIds = [];
+  for(var enemy = 1; enemy <= options.enemies; enemy++){
+    enemyIds.push(enemy);
+  }
+  return enemyIds;
+};
+
+var enemyIds = makeEnemies;
+
 var enemies = board.selectAll(".enemy")
                    .data(enemyIds,function(id){return id;});
 
+// Add enemies to board
 enemies.enter().append("svg:circle")
        .attr("class","enemy")
        .attr("stroke","#00A000")
@@ -37,7 +96,7 @@ enemies.enter().append("svg:circle")
        .attr("stroke-width", 1e-6)
        .attr("fill","none")
     .transition()
-       .duration(1000)
+       .duration(3000)
        .attr("r",15)
        .attr("stroke-opacity",1)
        .attr("stroke-width",1.1);
@@ -46,7 +105,7 @@ enemies.attr("x",function(id){
   return 10 * id;
 });
 
-
+// Allow enemies to move randomly around the board
 var enemyMethods = {
   moveEnemy : function(){
     d3.selectAll(".enemy")
@@ -59,68 +118,57 @@ var enemyMethods = {
   }
 };
 
-setInterval(enemyMethods.moveEnemy,1750);
 
 
-var playerSpecs = {
-  shape: "path",
-  path:   "M 9  1"
-        + "L 17 26"
-        + "L 9  22"
-        + "L 1  26"
-        + "L 9  1",
-  id: 0
+// Check for collisions between player and any enemies
+//     Current collision detection implementation is inefficient and inaccurate
+//     It could be improved by comparing player positions to the line the enemies travel each tick
+var checkCollisions = function() {
+  var nodes = d3.selectAll(".enemy");
+  var player = d3.select(".playerContainer");
+  var playerX = 1*player.attr("x") + (player.attr("width") / 2);
+  var playerY = 1*player.attr("y") + (player.attr("height") / 2);
+  var playerR = 8; // modeling player as circle to simplify calculations
+  for (var i = 0; i < nodes.length; i++) {
+    var enemy = d3.select(nodes[0][i]);
+    var enemyX = enemy.attr("cx");
+    var enemyY = enemy.attr("cy");
+    var enemyR = enemy.attr("r");
+    var distance = Math.sqrt(Math.pow((enemyY - playerY),2) + Math.pow((enemyX - playerX),2));
+    console.log(distance);
+    if (distance < enemyR + playerR) {
+      collide();
+    }
+  }
 };
 
-var playerContainer = board.append("svg:svg")
-                           .attr("class","playerContainer")
-                           .attr("width","20")
-                           .attr("height","28")
-                           .attr("x",options.width/2)
-                           .attr("y",options.height/2);
+// On collision, reset score and increase collisions count
+var collide = function() {
+  console.log("collision!");
+  window.stats.current = 0;
+  window.collisions++;
+  updateScores([stats.current, stats.highScore, stats.collisions]);
+};
 
-var player = board.selectAll(".player").data([player]);
+// Update scores frequently
+var updateScores = function(scores){
+  d3.selectAll(".scoreboard > div > span")
+    .data(scores)
+    .text(function(d){
+            return d;
+          });
+};
 
-
-playerContainer.append("svg:" + playerSpecs.shape)
-               .attr("d",playerSpecs.path)
-               .attr("class","player")
-               .attr("stroke","#00D000")
-               .attr("stroke-opacity",1e-6)
-               .attr("stroke-width", 1e-6)
-               .attr("fill","#00D000")
-               .attr("fill-opacity",0.25)
-             .transition()
-               .duration(1000)
-               .attr("stroke-opacity",1)
-               .attr("stroke-width",2);
-
-
-var drag = d3.behavior.drag().on("drag",function(e){
-  d3.select(this)
-    .attr("x",d3.event.x)
-    .attr("y",d3.event.y);
-});
-
-d3.select(".playerContainer").call(drag);
+var updates = function(){
+  // No particular reason for 683.
+  stats.current += Math.floor(683 * Math.random());
+  if(stats.current > stats.highScore){
+    stats.highScore = stats.current;
+  }
+  updateScores([stats.current, stats.highScore, stats.collision]);
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+setInterval(checkCollisions, 10);
+setTimeout(function(){return setInterval(updates,300)},3000)
+setTimeout(function(){return setInterval(enemyMethods.moveEnemy,1250)}, 1750);
